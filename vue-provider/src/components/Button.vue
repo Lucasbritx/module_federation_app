b845d460214ddebd9d4fc7db21abedd5f8445f4c
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-05-25 23:48:12 -0300

    WIP new vue provider

---
+++ vue-provider/src/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <button @click="handleClick">{{ label }}</button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    label: {
+      type: String,
+      default: 'Click Me'
+    }
+  },
+  methods: {
+    handleClick() {
+      this.$emit('click');
+    }
+  }
+}
+</script>
+
+<style scoped>
+button {
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+}
+</style>
