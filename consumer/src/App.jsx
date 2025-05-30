09efb92a27b8667a168af7d33f04a1884cdb95f0
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-05-29 22:52:08 -0300

    feat: created vueWrapper to import correctly vue button

--- consumer/src/App.jsx
+++ consumer/src/App.jsx
@@ -1,15 +1,29 @@
-import React, { Suspense } from "react";
-import Button from "../../vue-provider/src/components/Button.vue";
+import React, { useEffect, useState } from "react";
+import VueWrapper from "./VueWrapper.jsx";
 const RemoteButton = React.lazy(() => import("provider/Button"));
 
+const loadVueComponent = async () => {
+  const module = await import("vueApp/Button");
+  return module.default;
+};
+
 function App() {
+  const [VueButton, setVueButton] = useState(null);
+
+  useEffect(() => {
+    loadVueComponent().then(setVueButton);
+  }, []);
   return (
     <div>
       <span>Hello from Consumer Application</span>
       <RemoteButton onClick={() => alert("Federated button clicked!")}>
         Federated Button
       </RemoteButton>
-      <Button onClick={() => alert("Vue button clicked!")}>Vue Button</Button>
+      {VueButton ? (
+        <VueWrapper component={VueButton} props={{ text: "Clique aqui" }} />
+      ) : (
+        <p>Carregando botão Vue...</p>
+      )}
     </div>
   );
 }
